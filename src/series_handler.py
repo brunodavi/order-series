862d122b734cdfa0c501cc973cdc2b3b1e1405a0
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2023-04-05 14:10:18 -0300

    feat: implementado e testado get_series_name

--- src/series_handler.py
+++ src/series_handler.py
@@ -1,2 +1,53 @@
-def get_series_name(series):
-    ...
+from re import sub, IGNORECASE
+
+
+PATTERNS = [
+    r'^{counter}[_\W]+(.+?)\.\w{{3,4}}$',
+    r'^(.+?)[_\W]+{counter}\.\w{{3,4}}$',
+]
+
+LABEL_COUNTER = [
+    r'S\d+E\d+',
+    r'Episódios? \d+',
+    r'Ep \d+',
+    r'\d+'
+]
+
+
+def get_series_name(videos):
+    if not isinstance(videos, list):
+        raise ValueError('Não foi passado uma lista')
+
+    if len(videos) == 0:
+        raise ValueError('')
+
+    series_names = set()
+
+    for video_name in videos:
+        if not isinstance(video_name, str):
+            raise ValueError('Valor passado não é uma string')
+
+        value = video_name
+
+        for pattern in PATTERNS:
+            counter = '|'.join(LABEL_COUNTER)
+            counter = f'(?:{counter})'
+            pattern = pattern.format(counter=counter)
+
+            value = sub(
+                pattern,
+                r'\1',
+                video_name,
+                flags=IGNORECASE,
+            )
+
+            if value != video_name:
+                break
+
+        if value != video_name:
+            series_names.add(value)
+
+    if len(series_names) == 0:
+        raise ValueError('Não foi encontrada nenhuma serie')
+
+    return series_names
--- test/test_get_series_name.py
+++ test/test_get_series_name.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from src.series_handler import get_series_name
 
 
-NAO_TEM_SERIES = 'Não foi encontrada nenhuma série'
+NAO_TEM_SERIES = 'Não foi encontrada nenhuma serie'
 
 NAO_EH_LISTA = 'Não foi passado uma lista'
 VALOR_INVALIDO = 'Valor passado não é uma string'
@@ -15,7 +15,7 @@ class TestGetSeriesName(TestCase):
             'Serie1 2.mkv',
             'S1E1_Serie2.mov',
             'Serie2 - S1E1.wmv',
-            'Serie3_ -dgEpisódio 01.avi',
+            'Serie3_ -Episódio 01.avi',
             'Serie3, - Episódio 02.webm',
             'Episódio 03 - Serie3.mp4',
             'Episódio 01 - Uma Boa Serie 4.avi',
@@ -27,24 +27,42 @@ class TestGetSeriesName(TestCase):
             '03 - Uma Serie 5.mkv',
             'Uma Serie 5 S01E04.mp4',
             'Serie.6 S01E01.mp4',
-            'Serie.6 Episódio 2.mp4',
+            'Serie.6_Episódio 2.mp4',
             'Serie.6 Episódio 03.mp4',
         ]
 
-        esperado = [
+        esperado = {
             'Serie1',
             'Serie2',
             'Serie3',
             'Uma Boa Serie 4',
             'Uma Serie 5',
             'Serie.6',
-        ]
+        }
 
         resultado = get_series_name(entrada)
 
         self.assertEqual(resultado, esperado)
 
-    def test_erro_nao_existe_series(self):
+    def test_ignora_nomes_que_nao_sao_seriess(self):
+        entrada = [
+            '0',
+            'Serie1 - Ep 01.mp4',
+            'Serie1 - Episódio 2.avi',
+            'Serie1Ep01.wmv',
+            'S1E1 - Serie2.mp4',
+        ]
+
+        esperado = {
+            'Serie1',
+            'Serie2',
+        }
+
+        resultado = get_series_name(entrada)
+
+        self.assertEqual(resultado, esperado)
+
+    def test_erro_nao_tem_series_na_lista(self):
         entrada = [
             'jfjeixniejdk',
             'nckdndkdnkd',
