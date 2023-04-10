f68cba506986d6a331b180c2cbeaf8c97656bad1
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2023-04-09 21:33:27 -0300

    feat: implementação da main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
-from src.series_handler import get_series_name
+from pathlib import Path
+
 from src.cli import get_parser
+from src.series_handler import (
+    get_series_name,
+    filter_files_exists,
+)
 
 
 def main(args = None):
@@ -7,4 +12,30 @@ def main(args = None):
     args = parser.parse_args(args)
 
     path_list = args.video_series
-    
+    path_list_exists = filter_files_exists(path_list)
+
+    path_list_exists = list(
+        map(str, path_list_exists)
+    )
+
+    series_names_list = get_series_name(path_list_exists)
+
+    series_names_list = sorted(
+        series_names_list,
+        reverse=True,
+        key=len,
+    )
+
+    for serie_name in series_names_list:
+        dir_name = Path(serie_name)
+        dir_name.mkdir()
+
+        for video_path in Path().iterdir():
+            if not video_path.is_dir():
+                if serie_name in video_path.name:
+                    new_path = dir_name / video_path.name
+                    video_path.rename(new_path)
+
+
+if __name__ == "__main__":
+    main()
